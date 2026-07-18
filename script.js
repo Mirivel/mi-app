6a820bacccda35178604f1acd9baf40aa65fd7c5
fabiola vazquez <fabis.mel006@gmail.com>
2026-07-17 20:25:14 -0600

    Agrego formulario de contacto

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function cambiarMensaje() {
+function mostrarMensaje() {
+  let nombre = document.getElementById("nombre").value;
+  let mensaje = document.getElementById("mensajeTexto").value;
+
+  document.getElementById("resultado").innerHTML =
+    "Hola " + nombre + ", recibimos tu mensaje: " + mensaje;
+}function cambiarMensaje() {
   document.getElementById("mensaje").innerHTML = "¡Mi primera función con JavaScript!";
 }
